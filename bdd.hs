424385b9f3c445376d11d9bc8a0fa826acbf1f3c
No Name <49948225+playking@users.noreply.github.com>
2025-04-24 21:15:31 +0300

    Add files via upload

--- bdd.hs
+++ bdd.hs
@@ -5,12 +5,10 @@ trueBDD, falseBDD :: BDD
 trueBDD = Leaf True
 falseBDD = Leaf False
 
--- негатив 
-notBDD:: BDD -> BDD
+notBDD :: BDD -> BDD
 notBDD (Leaf b) = Leaf (not b)
 notBDD (Node var low high) = Node var (notBDD low) (notBDD high)
 
--- конъюкция 
 andBDD :: BDD -> BDD -> BDD
 andBDD (Leaf True) bdd = bdd
 andBDD (Leaf False) _  = falseBDD
@@ -27,18 +25,73 @@ andBDD (Node v1 l1 h1) (Node v2 l2 h2)
 orBDD :: BDD -> BDD -> BDD
 orBDD a b = notBDD $ andBDD (notBDD a) (notBDD b)
 
+andList :: [BDD] -> BDD
+andList = foldr andBDD trueBDD
 
-bdd1 = Node (1,1) (Leaf True) (Node (1,2) (Leaf False) (Leaf True))
-bdd2 = Node (0,1) (Leaf False) (Leaf True) 
+orList :: [BDD] -> BDD
+orList = foldr orBDD falseBDD
 
-testNotBDD = notBDD bdd1
+notList :: [BDD] -> BDD
+notList = andList . map notBDD
 
-testAndBDD = andBDD bdd1 bdd2
+pickOne :: [a] -> [(a, [a])]
+pickOne [] = []
+pickOne (x:xs) = (x, xs) : [ (y, x:ys) | (y, ys) <- pickOne xs ]
 
-testOrBDD= orBDD bdd1 bdd2
+exactlyOne :: [BDD] -> BDD
+exactlyOne vars = orList [ andBDD v (notList vs) | (v, vs) <- pickOne vars ]
+
+atMostOne :: [BDD] -> BDD
+atMostOne [] = trueBDD
+atMostOne (v:vs) = andList [ notBDD (andBDD v v') | v' <- vs ] `andBDD` atMostOne vs
+
+bddVar :: (Int, Int) -> BDD
+bddVar pos = Node pos falseBDD trueBDD
+
+-- ровно один ферщь в строке 
+rowCheck :: Int -> Int -> BDD
+rowCheck n i = exactlyOne [ bddVar (i, j) | j <- [0..n-1] ]
+
+-- один ферзь в столбце
+colCheck :: Int -> Int -> BDD
+colCheck n j = exactlyOne [ bddVar (i, j) | i <- [0..n-1] ]
+
+-- Один ферзь в диагонале 
+diagCheck :: Int -> BDD
+diagCheck n = andList $
+  [ atMostOne [ bddVar (i,j) | i <- [0..n-1], j <- [0..n-1], i - j == d ]
+  | d <- [-n+1..n-1] ] ++
+  [ atMostOne [ bddVar (i,j) | i <- [0..n-1], j <- [0..n-1], i + j == d ]
+  | d <- [0..2*n-2] ]
+
+nQueensBDD :: Int -> BDD
+nQueensBDD n = andList $
+  [ rowCheck n i | i <- [0..n-1] ] ++
+  [ colCheck n j | j <- [0..n-1] ] ++
+  [ diagCheck n ]
 
 main :: IO ()
 main = do
-  putStrLn $ "notBDD" ++ show testNotBDD
-  putStrLn $ "andBDD" ++ show testAndBDD
-  putStrLn $ "orBDD" ++ show testOrBDD
+  let bdd = nQueensBDD 4
+  print bdd
+
+
+
+
+
+
+
+-- bdd1 = Node (1,1) (Leaf True) (Node (1,2) (Leaf False) (Leaf True))
+-- bdd2 = Node (0,1) (Leaf False) (Leaf True) 
+
+-- testNotBDD = notBDD bdd1
+
+-- testAndBDD = andBDD bdd1 bdd2
+
+-- testOrBDD= orBDD bdd1 bdd2
+
+-- main :: IO ()
+-- main = do
+--   putStrLn $ "notBDD" ++ show testNotBDD
+--   putStrLn $ "andBDD" ++ show testAndBDD
+--   putStrLn $ "orBDD" ++ show testOrBDD
